826e41bbda5c5eec00c78fe6d9b7e4d05645d7f6
Rudr Bansal <rudrabhishekbansal@gmail.com>
2021-12-07 11:22:09 +0530

    Fonts added Tab bar added

---
+++ Hi5/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  Hi5
+//
+//  Created by Rudr Bansal on 07/12/21.
+//
+
+import Foundation
+
+class Constants {
+    
+    enum Storyboards: String {
+        case dashboardViewController = "DashboardViewController"
+        case offersViewController = "OffersViewController"
+        case rewardsViewController = "RewardsViewController"
+        case discoverViewController = "DiscoverViewController"
+        case walletViewController = "WalletViewController"
+    }
+}
---
+++ Hi5/Home/WalletViewController.swift
@@ -0,0 +1,21 @@
+//
+//  WalletViewController.swift
+//  Hi5
+//
+//  Created by Rudr Bansal on 07/12/21.
+//
+
+import UIKit
+
+class WalletViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
